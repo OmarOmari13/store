f727667c2faa51b2fee8ca5e7e446ed1e116b8b5
Mohammad Omar <omaromari1375@gmail.com>
2022-09-25 16:54:11 +0430

    new commit

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,5 +1,5 @@
 
-import { useEffect } from 'react';
+
 import {CardContainer, CardContent, Img} from './card.styles'
 
 
--- src/components/main/main.component.jsx
+++ src/components/main/main.component.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom"
+
 import {MainContainer , ButtonLink, Paragraph} from './main.styles'
 import Button from "../button/button.component"
 
